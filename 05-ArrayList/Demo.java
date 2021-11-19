9ff5aeecce63e2be2a1b0db2537dc0e11e1ef262
Istab Rakin <irakin30@stuy.edu>
2021-11-19 10:15:59 -0500

    yes

---
+++ 05-ArrayList/Demo.java
@@ -0,0 +1,7 @@
+public class Demo{
+  public static void main(String[]args){
+    String s = "123";
+    int x = Integer.parseInt(s);
+    System.out.println(x+1);//you can't add unless it is an int
+  }
+}
