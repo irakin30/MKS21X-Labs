4b73cb1765914b31bcc25901bba2ea0109ce83a1
Istab Rakin <irakin30@stuy.edu>
2021-11-23 10:16:25 -0500

    yes

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 public class Triangles {
     public static void main(String[] args) {
           try {
-              File data = new File("data.txt");
+              File data = new File(args[0]);
               Scanner input = new Scanner(data);
-              int[] nums = new int[3];
-              int count = 0;
-              while(input.hasNextInt()) {
-                  for (int i = 0; i < 3; i++) {
-                      nums[i] = input.nextInt();
-                  }
-                  if (isTriangle(nums)) {
-                      count++;
-                  }
+              if (Integer.parseInt(args[1]) == 1) {
+                  byRow(input);
               }
-              System.out.println(count);
+              else {
+                byColumn(input);
+              }
+              input.close();
           }
           catch (FileNotFoundException e) {
+              //maybe throw an exception lol
           }
     }
 
@@ -28,5 +25,45 @@ public class Triangles {
         int b = nums[1];
         int c = nums[2];
         return (a + b > c) && (b + c > a) && (a + c > b);
+    }
+
+    public static void byRow(Scanner input) {
+        int[] nums = new int[3];
+        int count = 0;
+        while(input.hasNextInt()) {
+            for (int i = 0; i < 3; i++) {
+                nums[i] = input.nextInt();
+            }
+            if (isTriangle(nums)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void byColumn(Scanner input) {
+        int count = 0;
+        int i = 0;
+        int[] a = new int[3];
+        int[] b = new int[3];
+        int[] c = new int[3];
+        while(input.hasNextInt) {
+             if (int i > 2) {
+               i = 0;
+               if (isTriangle[a]) {
+                 count++;
+               }
+               if (isTriangle[b]) {
+                 count++;
+               }
+               if (isTriangle[c]) {
+                 count++;
+               }
+             }
+             a[i] = input.nextInt();
+             b[i] = input.nextInt();
+             c[i] = input.nextInt();
+        }
+        System.out.println(count);
     }
 }
