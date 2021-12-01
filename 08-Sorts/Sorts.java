b8c07a1d87ad16dcb1b1b34e89c3a97c6e5ebd5c
Istab Rakin <irakin30@stuy.edu>
2021-12-01 10:06:55 -0500

    Only have to check for early termination now

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -13,7 +13,7 @@ public class Sorts{
     public static void bubbleSort(int[] data){
         int temp;
         for (int i = 0; i < data.length - 1; i++) {
-            for(int j = 0; j < data.length - i; j++) {
+            for(int j = 0; j < data.length - (i + 1); j++) {
                 if (data[j] > data[j + 1]) {
                     temp = data[j];
                     data[j] = data[j + 1];
